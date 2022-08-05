5b4ef05374461419944287daa1025fb80650cadc
shubhamchouhan23 <63260303+shubhamchouhan23@users.noreply.github.com>
2022-08-05 15:30:47 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,4 +1,26 @@
+/* 
+$myshell
+> BOOK=/home/myacct/book
+> export BOOK
+> envprint
+HOME=/home/myacct
+LOGNAME=myacct
+PATH=/bin:/usr/bin
+PS1=$
+PS2=>
+BOOK=/home/myacct/book
+*/
+/* For this I made myshell and export variables to it using "export"
+And then I made /.bashrc and export the variables into it and run it through source~
+*/
+
+// I run these commands -->
+
+
+/*
 sudo nano /etc/profile.d/myshell.sh
 sudo nano ~/.bashrc
 source ~/.bashrc
 printenv HOME
+*/
+
